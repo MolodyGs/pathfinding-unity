125aed1dc8e411acb8f2b2416331433a05517b7f
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-22 19:18:52 -0300

    chore: add logs for App.cs

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -7,8 +7,7 @@ public class App : MonoBehaviour
 
   async void Start()
   {
-    bool response = await TilesController.Instance.ReadTilesData();
-    Debug.Log("Tiles data loaded: " + response);
+    bool response = await TilesController.ReadTilesData();
   }
 
 }
