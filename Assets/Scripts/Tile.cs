e9ae05cc6f4042078cc7a8727dddfd8243783587
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-23 14:54:52 -0300

    feat<Tile>: add attributes

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -10,6 +10,13 @@ public class Tile : MonoBehaviour
   public float x;
   public float z;
   public bool blocked;
+  public int gCost = 0;
+  public int hCost = 0;
+  public int fCost = 0;
+
+  public bool active;
+
+  public GameObject parent;
 
   public Tile(float x, float z, bool blocked = false)
   {
@@ -23,6 +30,13 @@ public class Tile : MonoBehaviour
     x = transform.position.x;
     z = transform.position.z;
     ChangeState(blocked);
+  }
+
+  void OnMouseDown()
+  {
+    Tile tile = GetComponent<Tile>();
+    Debug.Log("Haz hecho click en: Tile: " + tile.x + ", " + tile.z + ", " + tile.blocked);
+    Input.setTile(gameObject);
   }
 
   public void ChangeState(bool state)
@@ -42,5 +56,34 @@ public class Tile : MonoBehaviour
       this.transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
       this.transform.localScale = new Vector3(transform.localScale.x, 0.9f, transform.localScale.z);
     }
+  }
+
+  public void SetGCost(int cost)
+  {
+    gCost = cost;
+    fCost = gCost + hCost;
+  }
+
+  public void SetHCost(int cost)
+  {
+    hCost = cost;
+    fCost = gCost + hCost;
+  }
+
+  public void Reset()
+  {
+    gCost = 0;
+    hCost = 0;
+    fCost = 0;
+    active = false;
+    parent = null;
+    GetComponent<Renderer>().material.color = Color.white;
+  }
+
+  public void SetPath()
+  {
+    GetComponent<Renderer>().material.color = Color.blue;
+    if (parent == null) return;
+    parent.GetComponent<Tile>().SetPath();
   }
 }
