eb6e7f77d000f0a3f008eec53af10b0a2a0f717e
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-24 18:33:10 -0300

    docs<InputController>: add comments

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -1,16 +1,23 @@
-
 using UnityEngine;
 
+/// <summary> 
+/// Lectura de la interacción del usuario con los tiles.
+/// </summary>
 public static class InputController
 {
   public static GameObject origin { get; set; }
   public static GameObject destination { get; set; }
 
+  /// <summary>
+  /// Asigna el tile seleccionado por el usuario como origen o destino.
+  /// </summary>
   public static async void SetTile(GameObject tile)
   {
 
+    // Si el tile está bloqueado, no se puede seleccionar.
     if (tile.GetComponent<Tile>().blocked) return;
 
+    // Si no hay un origen, se asigna el tile seleccionado como origen.
     if (origin == null)
     {
       origin = tile;
@@ -18,17 +25,22 @@ public static class InputController
       return;
     }
 
+    // Si no hay un destino, se asigna el tile seleccionado como destino.
     if (destination == null)
     {
       destination = tile;
       tile.GetComponent<Renderer>().material.color = Global.BLUE;
+
+      // Se ejecuta el pathfinding y el movimiento.
       await PathfindingController.Path();
       await MoveController.Move();
       return;
     }
+
+    // Si ya hay un origen y un destino, se reinician los tiles y se asigna el nuevo tile seleccionado como destino.
     PathfindingController.ResetTiles();
-    origin.GetComponent<Renderer>().material.color = Global.WHITE;
-    destination.GetComponent<Renderer>().material.color = Global.WHITE;
+    origin.GetComponent<Tile>().Reset();
+    destination.GetComponent<Tile>().Reset();
     origin = destination;
     destination = tile;
     await PathfindingController.Path();
