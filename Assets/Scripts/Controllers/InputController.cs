d6fdf56f48897b5dcc352a783f1ddbd17b3772c9
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-24 00:03:48 -0300

    feat: the movement start at the end of the last movement

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -6,7 +6,7 @@ public static class InputController
   public static GameObject origin { get; set; }
   public static GameObject destination { get; set; }
 
-  public static void setTile(GameObject tile)
+  public static async void SetTile(GameObject tile)
   {
 
     if (tile.GetComponent<Tile>().blocked) return;
@@ -14,22 +14,25 @@ public static class InputController
     if (origin == null)
     {
       origin = tile;
-      tile.GetComponent<Renderer>().material.color = Color.green;
+      tile.GetComponent<Renderer>().material.color = Global.GREEN;
       return;
     }
 
     if (destination == null)
     {
       destination = tile;
-      tile.GetComponent<Renderer>().material.color = Color.blue;
-      PathfindingController.Path();
+      tile.GetComponent<Renderer>().material.color = Global.BLUE;
+      await PathfindingController.Path();
+      await MoveController.Move();
       return;
     }
     PathfindingController.ResetTiles();
-    origin.GetComponent<Renderer>().material.color = Color.white;
-    destination.GetComponent<Renderer>().material.color = Color.white;
-    origin = tile;
-    destination = null;
-    tile.GetComponent<Renderer>().material.color = Color.green;
+    origin.GetComponent<Renderer>().material.color = Global.WHITE;
+    destination.GetComponent<Renderer>().material.color = Global.WHITE;
+    origin = destination;
+    destination = tile;
+    await PathfindingController.Path();
+    await MoveController.Move();
+    tile.GetComponent<Renderer>().material.color = Global.GREEN;
   }
 }
