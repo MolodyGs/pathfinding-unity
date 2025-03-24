d0349e6d7fb78348debea3f1db0bf989098a3f89
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-24 18:32:51 -0300

    docs<App>: add comments

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2,18 +2,21 @@ using System.IO;
 using System;
 using UnityEngine;
 
+/// <summary> 
+/// Clase inicial la cual ejecuta la lectura del archivo tiles.json a partir de la clase TilesController.
+/// </summary>
 public class App : MonoBehaviour
 {
-
   GameObject[] tiles;
   public async void Start()
   {
+    //Obtiene los tiles a partir de la lectura del archivo tiles.json
     tiles = await TilesController.ReadTilesData();
-    for (int i = 0; i < tiles.Length; i++)
-    {
-      Debug.Log(tiles[i].transform.position);
-    }
+
+    // Se asignan los tiles al PathfindingController
     PathfindingController.SetTiles(tiles);
+
+    // Luego de la asignación, se espera la interacción del usuario para iniciar el pathfinding por medio de la clase InputController
   }
 }
 
