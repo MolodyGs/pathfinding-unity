df56d9e9fbfcb94efbe0e1a102faefa9033df61d
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-24 00:02:05 -0300

    feat: add global colors

---
+++ Assets/Scripts/Global/Colors.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class Global 
+{
+
+  public static readonly Color RED = new Color(1.0f, 0.1f, 0.1f);
+  public static readonly Color WHITE = new Color(1.0f, 1.0f, 1.0f);
+  public static readonly Color BLUE = new Color(0.1f, 0.1f, 1.0f);
+  public static readonly Color GREEN = new Color(0.1f, 1.0f, 0.1f);
+  public static readonly Color YELLOW = new Color(1.0f, 1.0f, 0.1f);
+
+}
