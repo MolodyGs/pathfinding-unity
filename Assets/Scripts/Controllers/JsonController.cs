0a91eb8c52ee50be5b2376e8f511ec03728ab1d4
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-24 18:33:29 -0300

    docs<JsonController>: add comments

--- Assets/Scripts/Controllers/JsonController.cs
+++ Assets/Scripts/Controllers/JsonController.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 using System.IO;
 using System.Threading.Tasks;
 
-/// <summary> Lectura y manejo general de archivo JSON.</summary>
+/// <summary>
+/// Lectura y manejo general de archivo JSON.
+/// </summary>
 public static class JsonController
 {
 
@@ -24,10 +26,12 @@ public static class JsonController
       return default;
     }
 
+    // Obtiene el contenido del archivo JSON
     string json = await File.ReadAllTextAsync(path);
 
     try
     {
+      // Obtiene la información serializando el JSON con la clase variable "Structure"
       Structure data = JsonUtility.FromJson<Structure>(json);
       Debug.Log("Lectura exitosa!");
       return data;
@@ -39,24 +43,25 @@ public static class JsonController
     }
   }
 
+  /// <summary> Genera una archivo JSON a partir de "data".</summary>
+  /// <param name="dir">La dirección parcial del archivo .json</param>
+  /// <param name="fileName">Nombre del archivo JSON (por ejemplo: file.json)</param>
+  /// <param name="data">Datos a almacenar.</param>
+  /// <param name="Structure">Clase que se usará para deserializar el JSON.</param>
+  /// <returns>Retorna true o false dependiendo de si hubo o no un error al generar el archivo.</returns>
   public static async Task<bool> CreateJson<Structure>(string dir, string fileName, Structure data)
   {
     Debug.Log("Creando archivo JSON...");
     string path = Path.Combine(dir, fileName);
-    string json = JsonUtility.ToJson(data, true);
-
-    // Asegurarse de que el directorio existe
     string directory = Path.GetDirectoryName(path);
-    if (!Directory.Exists(directory))
-    {
-      Directory.CreateDirectory(directory);
-    }
+
+    // Verificar si el directorio existe
+    if (!Directory.Exists(directory)) { Directory.CreateDirectory(directory); }
 
     // Guardar el archivo JSON
+    string json = JsonUtility.ToJson(data, true);
     await File.WriteAllTextAsync(path, json);
-
     Debug.Log($"JSON creado en: {path}");
-
     return true;
   }
 }
