aa2e8cff36637308ac48e74ea02be35eeb97ad2a
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-03-31 21:11:16 -0300

    chore: better colors

--- Assets/Scripts/ByGameObjects/Global/Colors.cs
+++ Assets/Scripts/ByGameObjects/Global/Colors.cs
@@ -4,7 +4,7 @@ public static class Global
 {
 
   public static readonly Color BLOCKED = new(0.2f, 0.2f, 0.2f);
-  public static readonly Color RED = new(1.0f, 0.1f, 0.1f);
+  public static readonly Color RED = new(1.0f, 0.4f, 0.4f);
   public static readonly Color WHITE = new(1.0f, 1.0f, 1.0f);
   public static readonly Color BLUE = new(0.1f, 0.1f, 1.0f);
   public static readonly Color GREEN = new(0.1f, 1.0f, 0.1f);
