d16d74a0733a1c9325c9b04ba96cab6ac59e1771
Ignacio Molina <ignacio.molina@alumnos.ucn.cl>
2025-04-03 04:57:53 -0300

    refactor: better folder structure, remove unused code and files

--- Assets/Scripts/ByGameObjects/Serializables/TileDTO.cs
+++
@@ -1,8 +0,0 @@
-
-[System.Serializable]
-public class TileDTO
-{
-  public float x;
-  public float z;
-  public bool blocked;
-}
--- Assets/Scripts/ByGameObjects/Serializables/TileListDTO.cs
+++
@@ -1,6 +0,0 @@
-
-[System.Serializable] // Clase contenedora para serializar la lista de Tiles
-public class TileListDTO
-{
-  public TileDTO[] tiles;
-}
